1b970c2e0508090ef40a6d7fba05656ce94fac42
Manntrix <Manish.mandal21@gmail.com>
2023-04-01 02:49:02 +0530

    link updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import './App.css';
-import {useState,useEffect} from 'react'
-import axios from 'axios'
+import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
   //creating IP state
-  const [ip, setIP] = useState('');
+  const [ip, setIP] = useState("");
 
   //creating function to load ip address from the API
-  const getData = async () => {
-    const res = await axios.get('https://geolocation-db.com/json/')
-    console.log(res.data);
-    setIP(res.data.IPv4)
-  }
-  
-  useEffect( () => {
-    //passing getData method to the lifecycle method
-    getData()
+  // const getData = async () => {
+  //   const res = await axios.get("https://geolocation-db.com/json/");
+  //   console.log(res.data);
+  //   setIP(res.data.IPv4);
+  // };
 
-  }, [])
+  // Updated Code
+
+  const getData = async () => {
+    const res = await axios.get("https://api.ipify.org/?format=json");
+    console.log(res.data);
+    setIP(res.data.ip);
+  };
+
+  useEffect(() => {
+    //passing getData method to the lifecycle method
+    getData();
+  }, []);
 
   return (
     <div className="App">
